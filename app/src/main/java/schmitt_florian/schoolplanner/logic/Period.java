3f04212b57bf665da1323c4cdeff7d98b59ca147
Florian Schmitt <florian.c.schmitt@gmail.com>
2017-03-20 19:26:53 +0100

    Period class now has some getters and implements the Comparable Interface to be sorted by the schoolHoursNo

--- app/src/main/java/schmitt_florian/schoolplanner/logic/Period.java
+++ app/src/main/java/schmitt_florian/schoolplanner/logic/Period.java
@@ -1,12 +1,14 @@
 package schmitt_florian.schoolplanner.logic;
 
 
+import android.support.annotation.NonNull;
+
 import java.util.GregorianCalendar;
 
 /**
  * The Period Class represents an Object in the Period SQL table and is usually returned by methods from the DatabaseHelper Interface
  */
-public class Period {
+public class Period implements Comparable<Period> {
     /**
      * numeric id of the period (unique)
      */
@@ -16,6 +18,11 @@ public class Period {
      * the {@link Subject} taught in this period
      */
     private Subject subject;
+
+    /**
+     * the school hour number the period is, e.g. '1' for the first school hour
+     */
+    private int schoolHourNo;
 
     /**
      * the time the period starts as GregorianCalendar
@@ -31,33 +38,105 @@ public class Period {
     /**
      * standard c'tor for Period class
      *
-     * @param id        numeric id of the period (unique)
-     * @param subject   the {@link Subject} taught in this period
-     * @param startTime the time the period starts as GregorianCalendar
-     * @param endTime   the time the period ends as GregorianCalendar
+     * @param id           numeric id of the period (unique)
+     * @param subject      the {@link Subject} taught in this period
+     * @param schoolHourNo the school hour number the period is, e.g. '1' for the first school hour
+     * @param startTime    the time the period starts as GregorianCalendar
+     * @param endTime      the time the period ends as GregorianCalendar
      */
-    public Period(int id, Subject subject, GregorianCalendar startTime, GregorianCalendar endTime) {
+    public Period(int id, Subject subject, int schoolHourNo, GregorianCalendar startTime, GregorianCalendar endTime) {
         this.id = id;
         this.subject = subject;
+        this.schoolHourNo = schoolHourNo;
         this.startTime = startTime;
         this.endTime = endTime;
     }
 
     /**
-     * constructs a Period from Id as int and times as Strings,
+     * constructs a Period like the standard c'tor but start and end times given as Strings,
      * note, that if this c'tor is used the Year, month, dayOfMonth fields in the GregorianCalendars will be initialized with 0 value
      *
-     * @param id        numeric id of the period (unique)
-     * @param subject   the {@link Subject} taught in this period
-     * @param startTime the time the period starts as String separated by '-' to look like HH-MM-SS
-     * @param endTime   the time the period ends as String separated by '-' to look like HH-MM-SS
+     * @param id           numeric id of the period (unique)
+     * @param subject      the {@link Subject} taught in this period
+     * @param schoolHourNo the school hour number the period is, e.g. '1' for the first school hour
+     * @param startTime    the time the period starts as String separated by '-' to look like HH-MM-SS
+     * @param endTime      the time the period ends as String separated by '-' to look like HH-MM-SS
      */
-    public Period(int id, Subject subject, String startTime, String endTime) {
+    public Period(int id, Subject subject, int schoolHourNo, String startTime, String endTime) {
         this.id = id;
+        this.subject = subject;
+        this.schoolHourNo = schoolHourNo;
         this.startTime = getTimeFromString(startTime);
         this.endTime = getTimeFromString(endTime);
     }
     //endregion
+
+    //region getters
+
+    /**
+     * gets id of the period
+     *
+     * @return numeric id of the period (unique)
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * gets the {@link Subject} taught in this period
+     *
+     * @return {@link Subject} taught in this period
+     */
+    public Subject getSubject() {
+        return subject;
+    }
+
+    /**
+     * the school hour number the period is
+     *
+     * @return school hour number the period is, e.g. '1' for the first school hour
+     */
+    public int getSchoolHourNo() {
+        return schoolHourNo;
+    }
+
+    /**
+     * gets the time the period starts
+     *
+     * @return the time the period starts as GregorianCalendar
+     */
+    public GregorianCalendar getStartTime() {
+        return startTime;
+    }
+
+    /**
+     * gets the time the period ends
+     *
+     * @return the time the period ends as GregorianCalendar
+     */
+    public GregorianCalendar getEndTime() {
+        return endTime;
+    }
+    //endregion
+
+    /**
+     * compares the schoolHourNo's of this and the given period, it can be used to sort a Period[] by the schoolHourNo's,
+     * e.g. by using the Arrays.sort(Period[] periods)
+     * method to get a sorted Periods[]
+     *
+     * @param period the period to compare to
+     * @return -1,0,1 if the schoolHourNo of this period is less, equal, greater than the schoolHourNo of the given period
+     */
+    @Override
+    public int compareTo(@NonNull Period period) {
+        if (this.schoolHourNo < period.schoolHourNo) {
+            return -1;
+        } else if (this.schoolHourNo == period.schoolHourNo) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 
     //region private methods
 
@@ -76,6 +155,5 @@ public class Period {
                 Integer.parseInt(source.split("-")[2])
         );
     }
-
     //endregion
 }
