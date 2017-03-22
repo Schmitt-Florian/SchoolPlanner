cec37ecbf9fd3389f5f99602a5d0b190109de856
Florian Schmitt <florian.c.schmitt@gmail.com>
2017-03-22 14:21:41 +0100

    moved MainActivity.java to activities package, added german translation for menu_main_drawer.xml #22

--- app/src/main/java/schmitt_florian/schoolplanner/activities/MainActivity.java
+++ app/src/main/java/schmitt_florian/schoolplanner/activities/MainActivity.java
@@ -1,16 +1,13 @@
-package schmitt_florian.schoolplanner.logic;
+package schmitt_florian.schoolplanner.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import schmitt_florian.schoolplanner.R;
@@ -24,7 +21,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -48,10 +44,9 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
