81f283c0153d8854284f0a57b3d3e51f521433e7
natascha.becker <natascha_becker@gmx.net>
2017-04-27 14:24:40 +0200

    Created Fragments

--- app/src/main/java/layout/SettingsFragment.java
+++ app/src/main/java/layout/SettingsFragment.java
@@ -13,12 +13,12 @@ import schmitt_florian.schoolplanner.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SettingsFragments.OnFragmentInteractionListener} interface
+ * {@link SettingsFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link SettingsFragments#newInstance} factory method to
+ * Use the {@link SettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingsFragments extends Fragment {
+public class SettingsFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +30,7 @@ public class SettingsFragments extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public SettingsFragments() {
+    public SettingsFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +40,11 @@ public class SettingsFragments extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragments.
+     * @return A new instance of fragment SettingsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SettingsFragments newInstance(String param1, String param2) {
-        SettingsFragments fragment = new SettingsFragments();
+    public static SettingsFragment newInstance(String param1, String param2) {
+        SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
