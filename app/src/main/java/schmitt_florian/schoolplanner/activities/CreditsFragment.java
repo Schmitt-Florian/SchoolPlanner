7e7e3f1e62fc8dd14878a2f24c9fe6c524cd407f
Darius <dww99@web.de>
2017-05-02 09:23:36 +0200

    Wrote send email

--- app/src/main/java/schmitt_florian/schoolplanner/activities/CreditsFragment.java
+++ app/src/main/java/schmitt_florian/schoolplanner/activities/CreditsFragment.java
@@ -26,6 +26,7 @@ public class CreditsFragment extends Fragment {
 
     }
 
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
