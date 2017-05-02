66c04dd82b1edbe6ae5b2b0bbc4c5891158ee500
marlon.drolsbach <drolsbachmarlon@gmail.com>
2017-05-02 09:03:29 +0200

    Removed test

--- app/src/main/java/schmitt_florian/schoolplanner/activities/SubjectsFragment.java
+++ app/src/main/java/schmitt_florian/schoolplanner/activities/SubjectsFragment.java
@@ -65,12 +65,8 @@ public class SubjectsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
-
-        new GuiHelper().fillListViewFromArray(view, R.id.subjects_listView, test);
-
         // Inflate the layout for this fragment
-        return view;
+        return inflater.inflate(R.layout.fragment_schedule, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
