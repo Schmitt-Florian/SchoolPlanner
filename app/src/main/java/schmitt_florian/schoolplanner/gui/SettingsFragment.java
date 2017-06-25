1e587784ba61b03b9df3b7bd191fdd4bbdf85601
Darius <dww99@web.de>
2017-06-25 10:05:07 +0200

    continued weekday editor

--- app/src/main/java/schmitt_florian/schoolplanner/gui/SettingsFragment.java
+++ app/src/main/java/schmitt_florian/schoolplanner/gui/SettingsFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -26,7 +27,12 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
     private OnFragmentInteractionListener mListener;
     private Settings settings;
     private View view;
-
+    private boolean weekdaySetMon;
+    private boolean weekdaySetTue;
+    private boolean weekdaySetWed;
+    private boolean weekdaySetThur;
+    private boolean weekdaySetFri;
+    private boolean weekdaySetSat;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -73,9 +79,64 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
                 readGui();
                 settings.saveSettings();
                 Toast.makeText(getContext(), R.string.string_settings_saved, Toast.LENGTH_SHORT).show();
+                getActivatedWeekdays();
         }
     }
 
+    private void getActivatedWeekdays() {
+        CheckBox mon = (CheckBox) getActivity().findViewById(R.id.settings_checkBox_for_monday);
+        CheckBox tue = (CheckBox) getActivity().findViewById(R.id.settings_checkBox_for_tuesday);
+        CheckBox wed = (CheckBox) getActivity().findViewById(R.id.settings_checkBox_for_wednesday);
+        CheckBox thur = (CheckBox) getActivity().findViewById(R.id.settings_checkBox_for_thursday);
+        CheckBox fri = (CheckBox) getActivity().findViewById(R.id.settings_checkBox_for_friday);
+        CheckBox sat = (CheckBox) getActivity().findViewById(R.id.settings_checkBox_for_saturday);
+
+        weekdaySetMon = mon.isChecked();
+
+        weekdaySetTue = tue.isChecked();
+
+        weekdaySetWed = wed.isChecked();
+
+        weekdaySetThur = thur.isChecked();
+
+        weekdaySetFri = fri.isChecked();
+
+        weekdaySetSat = sat.isChecked();
+
+        System.out.println(
+                "mon: " + weekdaySetMon + "\n" +
+                        "tue: " + weekdaySetTue + "\n" +
+                        "wed: " + weekdaySetWed + "\n" +
+                        "thur: " + weekdaySetThur + "\n" +
+                        "fri: " + weekdaySetFri + "\n" +
+                        "sat: " + weekdaySetSat + "\n"
+        );
+
+    }
+
+    public boolean isWeekdaySetMon() {
+        return weekdaySetMon;
+    }
+
+    public boolean isWeekdaySetTue() {
+        return weekdaySetTue;
+    }
+
+    public boolean isWeekdaySetWed() {
+        return weekdaySetWed;
+    }
+
+    public boolean isWeekdaySetThur() {
+        return weekdaySetThur;
+    }
+
+    public boolean isWeekdaySetFri() {
+        return weekdaySetFri;
+    }
+
+    public boolean isWeekdaySetSat() {
+        return weekdaySetSat;
+    }
 
     /**
      * This interface must be implemented by activities that contain this
